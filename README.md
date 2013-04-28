8e2b976fa4de1b5a72c6033fbf33c41ef3efefcb
David Raison <david@raison.lu>
2013-04-28 19:41:54 +0200

    added more information to the readme, split SphinxAdapter into an adapter and a result object that will be returned

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ SphinxServiceProvider
 
 This is a simple service provider for silex that ties the SphinxSearch-API in.
 
+A word of warning
+-----------------
+
+This is currently work in progress and not suitable for use in productive environments.
+
+The API may change often and erratically. Generally, I would advise against
+using this before version 1.0.0
+
+I you'd really like to use this productively, I recommend to choose a version
+and stick with it, i.e. set "require 'ttf/sphinx': '0.1.2'" in your composer.json
+
 
 Usage 
 -----
@@ -20,15 +31,44 @@ assumed "localhost:3312", you need to supply these parameters here:
     ));
 
 Following that, you would use it as any other service provider.
-The only available call at this moment is "search":
+Start off by using the search method on the provider, which will return
+a result object:
 
     $app->get('/search', function( Request $request ) use ($app) {
         $result = $app['sphinx']->search( $request->get('q'), 'myCatalog' );
-        return new Response( json_encode( $result ), 200 );
+        return new Response( (String) $result, 200 );
     });
+
+Coercing the $result object to a string will return a human readable 
+representation of your research results.
 
 In this case, a call request would look like:
 
     http://silex.local/search?q=cheese
 
-and return a json string with the raw sphinx result object.
+A more elaborate example would be:
+
+    $app->get('/search', function( Request $request ) use ($app) {
+        if( $result = $app['sphinx']->search( $request->get('q'), 'catalog' ) ) {
+            $indices = $result->fetchIndices();
+            
+            $query = 'Select description, tags from stand where fiorg = ?';
+            foreach( $indices as $index ) {
+                $entries = $app['db']->fetchAssoc( $query, array( $index ));
+            }
+            $app['twig']->render('search.twig',
+                array(
+                    'results' => $entries
+                )
+            );
+        }
+    });
+
+Passing parameters to the Sphinx API
+------------------------------------
+
+Note that you can also directly pass arguments to the sphinxsearch api. 
+Simply use them on the service:
+
+    $app['sphinx']->SetRankingMode( SPH_RANK_SPH04 );
+    $app['sphinx']->SetFieldWeights( array( 'tags' => 10, 'description' => 5) );
--- src/Ttf/SphinxAdapter.php
+++ src/Ttf/SphinxAdapter.php
@@ -11,11 +11,23 @@ namespace Ttf;
 class SphinxAdapter {
     
     private $_cl;   // reference to the client object
-    
+       
     public  function __construct( $host, $port ){
         $this->_cl = new \SphinxClient;
         $this->_cl->SetServer( $host, (int) $port );
         $this->_cl->SetMatchMode( SPH_MATCH_ANY );
+    }
+    
+    /**
+     * Overload the __call method to access SphinxSearch-API methods directly
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     */
+    public function __call( $method, $params ){
+        if( method_exists( $this->_cl, $method ) ){
+            return call_user_func_array( array( $this->_cl, $method ), $params);
+        }
     }
     
     /**
@@ -27,14 +39,85 @@ class SphinxAdapter {
     public function search( $query, $catalog ){
         $result = $this->_cl->Query( $query, $catalog );
         if( $result == false ) {
-            echo "Sphinx client oops: " . $this->_cl->GetLastError();
-            return false;
+            throw new Exception( "Sphinx client oops: " . $this->_cl->GetLastError());
         } else {
-            if( !empty($result['matches']) ) {
-               return $result;   
+            if( !empty($result['warning']) ) {
+                return $this->_cl->GetLastWarning();
             }
+            return new SphinxResult( $result );
         }
-     
+    }
+    
+}
+
+class SphinxResult {
+    
+    private $_rs;       // stores original resultset
+    private $_matches;  // stores result matches and their attributes
+    private $_meta;     // stores metadata
+    
+    public function __construct( Array $resultSet ) {
+        $this->_rs = $resultSet;
+        $this->_matches = $resultSet['matches'];
+        $this->_meta = array( 
+            'time' => $resultSet['time'],
+            'total' => $resultSet['total'],
+            'total_found' => $resultSet['total_found'],
+        );
+    }
+    
+    /**
+     *
+     * @return string A human readable string of your search results
+     */
+    public function __toString() {
+        $resultString = '';
+        foreach( $this->_matches as $match => $attrs ) {
+            $resultString .= sprintf( 'Document %d matches (weight: %d).<br/>', $match, $attrs['weight']);
+        }
+        $resultString .= sprintf('<p>Found %d result(s) in in %s seconds.</p>', $this->_meta['total_found'], $this->_meta['time']);
+        return $resultString;
+    }
+    
+    /**
+     * Return the number of results found
+     * @return integer The number of results found
+     */
+    public function numFound(){
+        return (int) $this->_meta['total_found'];
+    }
+    
+    /**
+     *
+     * @return array the original sphinx result object
+     */
+    public function dump(){
+        return $this->_rs;
+    }
+    
+    /**
+     * Show the words used in the search and their respective hits.
+     * These depend on the morphology used
+     * @return array of words and their hits
+     */
+    public function showWords(){
+        return $this->_rs['words'];
+    }
+    
+    /**
+     * Return all matches from the previous search
+     * @return array of all matches
+     */
+    public function fetchAll(){
+        return $this->_matches;
+    }
+    
+    /**
+     *
+     * @return array of index keys
+     */
+    public function fetchIndices(){
+        return array_keys( $this->_matches );
     }
     
 }
